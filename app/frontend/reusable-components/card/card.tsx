99a8768e49a6a9e43d6e2936fab92da151953379
skamath2000 <samanthakamath2000@gmail.com>
2025-05-09 10:49:11 -0500

    feat: add logo

--- app/frontend/reusable-components/card/card.tsx
+++ app/frontend/reusable-components/card/card.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react';
+import logo from '../../assets/logo.svg';  
 
 interface Props {
   children: ReactNode;
@@ -11,9 +12,9 @@ export function Card({ children, title, description }: Props) {
     <section className="bg-white p-10 shadow-card min-h-[400px] w-1/2 justify-self-center rounded-2xl border border-solid border-slate-200">
      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            alt="Your Company"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
+            alt="Mock company logo"
+            src={logo}
+            className="mx-auto h-16 w-auto"
           /> 
           <h2 className="mt-4 text-center text-2xl/9 font-bold tracking-tight text-gray-900 m-0 mb-4">
             {title}
--- app/frontend/reusable-components/radio-button-group/radion-button-group.tsx
+++ app/frontend/reusable-components/radio-button-group/radion-button-group.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const radioClasses = 'w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500';
+const radioClasses = 'w-4 h-4 text-indigo-500 bg-gray-100 border-gray-300 focus:ring-indigo-500';
 
 interface RadioButtonGroupProps {
     radioOptions: {
@@ -16,11 +16,11 @@ export const RadioButtonGroup = ({radioOptions, onChange}: RadioButtonGroupProps
         <ul className="p-3 space-y-1 text-sm text-gray-700 mb-10">
             {radioOptions.map((option, index) => 
                 <li key={`radio-list-option-${index}`}>
-                    <div className="flex p-2 rounded-md hover:bg-gray-100">
+                    <div className="flex p-2 items-center rounded-md hover:bg-gray-100">
                         <div className="flex items-center justify-center h-8 w-8">
                             <input onChange={onChange} id={`radio-option-${index}`} name="helper-radio" type="radio" value={option.value} className={radioClasses} />
                         </div>
-                        <div className="ms-4 flex">
+                        <div className="ms-4 flex p-2">
                             <label htmlFor={`radio-option-${index}`} className="items-center">{option.label}</label>
                         </div>
                     </div>
